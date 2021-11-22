fb0cd28a879cc2f218416c44db63d43919123e7c
40281172 <pmcteggart01@qub.ac.uk>
2021-11-22 20:59:41 +0000

    post and put added

--- db/studentQueries.js
+++ db/studentQueries.js
@@ -1,52 +1,141 @@
+const { query } = require("express");
 const dbPool = require("./config");
 
+const getStudentsQueryParamChecker = function (req) {
 
-const getStudentsQueryBuilder = function (studentQueryParams, moduleQueryParams, programQueryParams, flagQueryParams, meetingQueryParams) {
+    const addStudentQueryParams = function (req) {
 
-    var query;
+        const studentQueryParams = [req.query.studentNumber, req.query.fName, req.query.lName, req.query.DOB, req.query.admitTerm, req.query.email, req.query.currentLevel, req.query.startLevel];
+    
+        const getStudentSQLStrings = [
+          "student.studentNumber = " + "'" + req.query.studentNumber + "'",
+          "student.firstName = " + "'" + req.query.fName + "'",
+          "student.lastName = " + "'" + req.query.lName + "'",
+          "student.DOB = " + "'" + req.query.DOB + "'",
+          "student.admitTermID = " + "'" + req.query.admitTerm + "'",
+          "student.email = " + "'" + req.query.email + "'",
+          "student.currentLevel = " + "'" + req.query.currentLevel + "'",
+          "student.startLevel = " + "'" + req.query.startLevel + "'"
+        ]
+    
+        const studentParamsfiltered = getStudentSQLStrings.filter(function (element, index) {
+    
+          if (!studentQueryParams[index]) { return false } else return true;
+        })
+    
+        return studentParamsfiltered;
+    
+      }
+    
+      const addModuleQueryParams = function (req) {
+    
+        const moduleQueryParams = [req.query.moduleCatalogNumber, req.query.moduleDescription, req.query.moduleLevel, req.query.moduleAssessmentType, req.query.moduleSession, req.query.moduleCore, req.query.moduleTerm, req.query.moduleSubject];
+    
+        const moduleSQLStrings = [
+          "module.catalogNumber = " + "'" + req.query.moduleCatalogNumber + "'",
+          "module.moduleDescription = " + "'" + req.query.moduleDescription + "'",
+          "module.moduleLevel = " + "'" + req.query.moduleLevel + "'",
+          "module.assessmentTypeID = " + "'" + req.query.moduleAssessmentType + "'",
+          "module.sessionID = " + "'" + req.query.moduleSession + "'",
+          "student.core = " + "'" + req.query.moduleCore + "'",
+          "student.units = " + "'" + req.query.moduleTerm + "'",
+          "student.moduleTermID = " + "'" + req.query.moduleTerm + "'"
+        ]
+    
+        const moduleParamsfiltered = moduleSQLStrings.filter(function (element, index) {
+    
+          if (!moduleQueryParams[index]) { return false } else return true;
+        })
+    
+        return moduleParamsfiltered;
+    
+      }
+    
+      const addProgramQueryParams = function (req) {
+    
+        const programQueryParams = [req.query.programCode, req.query.programDescription, req.query.programAcademicLoad, req.query.programCareer, req.query.programAcademicOrg, req.query.programAcademicPlan, req.query.programStartTerm, req.query.programCampus];
+    
+        const programSQLStrings = [
+          "program.programCode = " + "'" + req.query.programCode + "'",
+          "program.programDescription = " + "'" + req.query.programDescription + "'",
+          "program.academicLoadID = " + "'" + req.query.programAcademicLoad + "'",
+          "program.careerID = " + "'" + req.query.programCareer + "'",
+          "program.academicOrgID = " + "'" + req.query.programAcademicOrg + "'",
+          "program.academicPlanID = " + "'" + req.query.programAcademicPlan + "'",
+          "program.startTermID = " + "'" + req.query.programStartTerm + "'",
+          "program.campusID = " + "'" + req.query.programCampus + "'"
+        ]
+    
+        const programParamsfiltered = programSQLStrings.filter(function (element, index) {
+    
+          if (!programQueryParams[index]) { return false } else return true;
+        })
+    
+        return programParamsfiltered;
+    
+      }
+    
+      const addFlagQueryParams = function (req) {
+    
+        const flagQueryParams = [req.query.flagID, req.query.flagDescription]
+    
+        const flagSQLStrings = [
+          "flag.flagID = " + "'" + req.query.flagID + "'",
+          "flag.flagDescription = " + "'" + req.query.flagDescription+ "'",
+        ]
+    
+        const flagParamsFiltered = flagSQLStrings.filter(function(element, index){
+          if (!flagQueryParams[index]) { return false } else return true;
+        })
+    
+        return flagParamsFiltered;
+    
+      }
+    
+      const addMeetingQueryParams = function(req) {
+    
+        const meetingQueryParams = [req.query.meetingID ,req.query.meetingDate, req.query.meetingOutcomeAdvice, req.query.meetingOutcomeProgressionIssue];
         
-     query = "SELECT student.studentNumber, student.firstName, student.lastName ,student.DOB, student.admitTermID, student.email, student.startLevel, student.currentLevel, studentModule.catalogNumber, program.programCode"
-     + " FROM student JOIN studentmodule ON student.studentNumber = studentmodule.studentNumber JOIN module ON studentmodule.catalogNumber = module.catalogNumber JOIN program ON module.programCode = program.programCode WHERE "
-     + studentQueryParams.join(' AND ')
-    + (moduleQueryParams.length > 0 ? " AND " + moduleQueryParams.join(' AND ') : "" )
-   + (programQueryParams.length > 0 ? " AND " + programQueryParams.join(' AND ') : "" )
-   + (flagQueryParams.length > 0 ? " AND " + flagQueryParams.join(' AND ') : "" )
-   + (meetingQueryParams.length > 0 ? " AND " + meetingQueryParams.join(' AND ') : "" )
+        const meetingSQLStrings = [
+          "meeting.meetingID = " + "'" + req.query.meetingID + "'",
+          "meeting.meetingDateTime = " + "'" + req.query.meetingDate + "'",
+          "meetingoutcome.adviceGivenID = " + "'" + req.query.meetingOutcomeAdvice + "'",
+          "meetingOutcome.progressionIssuesDiscussed = " + "'" + req.query.meetingOutcomeProgressionIssue + "'"
+            ]
+    
+            const meetingParamsFiltered = meetingSQLStrings.filter(function(element, index){
+              if (!meetingQueryParams[index]) { return false } else return true;
+            })
+            
+            return meetingParamsFiltered;
+      
+        }
+          
+          const studentQueryParams = addStudentQueryParams(req);
+          const moduleQueryParams = addModuleQueryParams(req);
+          const programQueryParams = addProgramQueryParams(req);
+      
+        if(req.method = "PUT") {putStudents(studentQueryParams, putStudentQueryBuilder)} else {
 
+         sqlQuery = getStudentsQueryBuilder(studentQueryParams, moduleQueryParams, programQueryParams,);
+         return sqlQuery;
 
-    console.log(query)
+        }
+        }
+
+const getStudentsQueryBuilder = function (studentQueryParams, moduleQueryParams, programQueryParams) {
+
+    //Search by student,module and program - studentflag and meeting records not yet added.
+    const query = "SELECT DISTINCT student.studentNumber, student.firstName, student.lastName, student.DOB, student.admitTermID, student.currentLevel, student.startLevel, module.programCode FROM student JOIN studentmodule ON student.studentNumber = studentmodule.studentNumber JOIN module ON studentmodule.catalogNumber = module.catalogNumber JOIN program ON module.programCode = program.programCode WHERE "  + studentQueryParams.join(' AND ')
+    + (moduleQueryParams.length > 0 ? "" + " AND " + moduleQueryParams.join(' AND ') : "" )
+   + (programQueryParams.length > 0 ? "" + " AND " + programQueryParams.join(' AND ') : "" )
+ 
     return query;
 }
 
-const getStudentsbyStudentNumber = function (studentNumber) {
+const getStudentsByQueryParams = function (queryParams, getStudentsQueryParamChecker) {
 
-    return new Promise(function (resolve, reject) {
-
-        var query = 'SELECT * FROM student WHERE studentNumber = ?;'
-      
-
-        dbPool.getConnection(function (err, connection) {
-
-            if (err) reject(err);
-
-            connection.query(query, studentNumber, function (err, data) {
-
-                connection.release();
-
-                if (err) reject(err);
-                else {
-                    const student = data;
-                    resolve(student);
-                }
-            })
-        })
-
-    })
-}
-
-const getStudentsByQueryParams = function (studentQueryParams, moduleQueryParams, programQueryParams, flagQueryParams, meetingQueryParams, getStudentsQueryBuilder) {
-
-    query = getStudentsQueryBuilder(studentQueryParams, moduleQueryParams, programQueryParams, flagQueryParams, meetingQueryParams);
+   const sqlQuery = getStudentsQueryParamChecker(queryParams);
   
     return new Promise(function (resolve, reject) {
 
@@ -54,7 +143,7 @@ const getStudentsByQueryParams = function (studentQueryParams, moduleQueryParams
 
             if (err) reject(err);
 
-            connection.query(query, function (err, data) {
+            connection.query(sqlQuery, function (err, data) {
 
                 connection.release();
 
@@ -67,6 +156,91 @@ const getStudentsByQueryParams = function (studentQueryParams, moduleQueryParams
         })
 
     })
+}
+
+const postStudentsQueryParamChecker = function(req) {
+    
+    const studentQueryParams = [req.query.studentNumber, req.query.fName, req.query.lName, req.query.DOB, req.query.admitTerm, req.query.email, req.query.currentLevel, req.query.startLevel];
+
+    sqlQuery = postStudentQueryBuilder(studentQueryParams);
+
+    return sqlQuery;
+}
+
+const postStudentQueryBuilder = function(studentQueryParams) {
+
+    
+var sqlQuery = "INSERT INTO student (`studentNumber`,`firstName`,`lastName`,`DOB`,`admitTermID`,`email`,`currentLevel`,`startLevel`) VALUES ("
++ "'" + studentQueryParams.join("' ,'") + "')";
+
+return sqlQuery;
+}
+
+const postStudents = function (queryParams, postStudentsQueryParamChecker) {
+
+    sqlQuery = postStudentsQueryParamChecker(queryParams);
+  console.log(sqlQuery);
+
+    return new Promise(function (resolve, reject) {
+
+        dbPool.getConnection(function (err, connection) {
+
+            if (err) reject(err);
+
+            connection.query(sqlQuery, function (err, data) {
+
+                connection.release();
+
+                if (err) reject(err);
+                else {
+                    const students = data;
+                    resolve(students);
+                }
+            })
+        })
+
+    })
+}
+
+  const putStudentQueryBuilder = function(studentQueryParams) {
+    
+
+   const studentNumber = studentQueryParams.filter(function(element) {
+    if(element.includes("student.studentNumber")) {return true} else return false;
+   })
+
+   const fieldsToUpdate = studentQueryParams.filter(function(element) {
+    if(element.includes("student.studentNumber")) {return false} else return true;
+   })
+
+   return "UPDATE student SET " + fieldsToUpdate.join(", ") + " WHERE " + studentNumber;
+
+      }
+
+const putStudents = function (studentQueryParams, putStudentQueryBuilder) {
+
+
+  const sqlQuery = putStudentQueryBuilder(studentQueryParams);
+console.log(sqlQuery)
+  return new Promise(function (resolve, reject) {
+
+      dbPool.getConnection(function (err, connection) {
+
+          if (err) reject(err);
+
+          connection.query(sqlQuery, function (err, data) {
+
+              connection.release();
+
+              if (err) reject(err);
+              else {
+                  const students = data;
+                  resolve(students);
+              }
+          })
+      })
+
+  })
 }
 
 
@@ -84,7 +258,12 @@ const getStudentsByQueryParams = function (studentQueryParams, moduleQueryParams
 
 module.exports = {
     getStudentsByQueryParams,
-    getStudentsbyStudentNumber,
-    getStudentsQueryBuilder
+    getStudentsQueryBuilder,
+    getStudentsQueryParamChecker,
+    postStudentsQueryParamChecker,
+    postStudentQueryBuilder,
+    postStudents,
+    putStudents,
+    putStudentQueryBuilder
 };
 
--- routes/students.js
+++ routes/students.js
@@ -15,206 +15,35 @@ function asyncErrHandler(callBack) {
   }
 }
 
-//GET STUDENTS BY STUDENT NUMBER
-router.get("/students/:studentNumber", async function (req, res, next) {
+//GET STUDENTS BY QUERY PARAMS
+router.get("/students", async function (req, res, next) {
+
   try {
-    const students = await studentQueryHandler.getStudentsbyStudentNumber(req.params.studentNumber);
+    const students = await studentQueryHandler.getStudentsByQueryParams(req, studentQueryHandler.getStudentsQueryParamChecker);
     res.send(students);
   } catch (err) {
     next(err)
   }
-}
-);
 
-//GET STUDENTS BY QUERY PARAMS
-router.get("/students", async function (req, res, next) {
+})
 
-  const returnInfo = [req.query.studentDetails, req.query.studentAcademicReacord];
-
-  const addStudentQueryParams = function (queryParams) {
-
-    const studentQueryParams = [req.query.studentNumber, req.query.fName, req.query.lName, req.query.DOB, req.query.admitTerm, req.query.email];
-
-    const studentSQLStrings = [
-      "student.studentNumber = " + "'" + req.query.studentNumber + "'",
-      "student.firstName = " + "'" + req.query.fName + "'",
-      "student.lastName = " + "'" + req.query.lName + "'",
-      "student.DOB = " + "'" + req.query.DOB + "'",
-      "student.admitTermID = " + "'" + req.query.admitTerm + "'",
-      "student.email = " + "'" + req.query.email + "'"
-    ]
-
-    const studentParamsfiltered = studentSQLStrings.filter(function (element, index) {
-
-      if (!studentQueryParams[index]) { return false } else return true;
-    })
-
-    return studentParamsfiltered;
-
+router.post("/students", async function (req, res, next) {
+  
+  try {
+    await studentQueryHandler.postStudents(req, studentQueryHandler.postStudentsQueryParamChecker);
+  } catch (err) {
+    next(err)
   }
+})
 
-  const addModuleQueryParams = function (queryParams) {
-
-    const moduleQueryParams = [req.query.moduleCatalogNumber, req.query.moduleDescription, req.query.moduleLevel, req.query.moduleAssessmentType, req.query.moduleSession, req.query.moduleCore, req.query.moduleTerm, req.query.moduleSubject];
-
-    const moduleSQLStrings = [
-      "module.catalogNumber = " + "'" + req.query.moduleCatalogNumber + "'",
-      "module.moduleDescription = " + "'" + req.query.moduleDescription + "'",
-      "module.moduleLevel = " + "'" + req.query.moduleLevel + "'",
-      "module.assessmentTypeID = " + "'" + req.query.moduleAssessmentType + "'",
-      "module.sessionID = " + "'" + req.query.moduleSession + "'",
-      "student.core = " + "'" + req.query.moduleCore + "'",
-      "student.units = " + "'" + req.query.moduleTerm + "'",
-      "student.moduleTermID = " + "'" + req.query.moduleTerm + "'"
-    ]
-
-    const moduleParamsfiltered = moduleSQLStrings.filter(function (element, index) {
-
-      if (!moduleQueryParams[index]) { return false } else return true;
-    })
-
-    return moduleParamsfiltered;
-
-  }
-
-  const addProgramQueryParams = function (queryParams) {
-
-    const programQueryParams = [req.query.programCode, req.query.programDescription, req.query.programAcademicLoad, req.query.programCareer, req.query.programAcademicOrg, req.query.programAcademicPlan, req.query.programStartTerm, req.query.programCampus];
-
-    const programSQLStrings = [
-      "program.programCode = " + "'" + req.query.programCode + "'",
-      "program.programDescription = " + "'" + req.query.programDescription + "'",
-      "program.academicLoadID = " + "'" + req.query.programAcademicLoad + "'",
-      "program.careerID = " + "'" + req.query.programCareer + "'",
-      "program.academicOrgID = " + "'" + req.query.programAcademicOrg + "'",
-      "program.academicPlanID = " + "'" + req.query.programAcademicPlan + "'",
-      "program.startTermID = " + "'" + req.query.programStartTerm + "'",
-      "program.campusID = " + "'" + req.query.programCampus + "'"
-    ]
-
-    const programParamsfiltered = programSQLStrings.filter(function (element, index) {
-
-      if (!programQueryParams[index]) { return false } else return true;
-    })
-
-    return programParamsfiltered;
-
-  }
-
-  const addFlagQueryParams = function (queryParams) {
-
-    const flagQueryParams = [req.query.flagID, req.query.flagDescription]
-
-    const flagSQLStrings = [
-      "flag.flagID = " + "'" + req.query.flagID + "'",
-      "flag.flagDescription = " + "'" + req.query.flagDescription+ "'",
-    ]
-
-    const flagParamsFiltered = flagSQLStrings.filter(function(element, index){
-      if (!flagQueryParams[index]) { return false } else return true;
-    })
-
-    return flagParamsFiltered;
-
-  }
-
-  const addMeetingQueryParams = function(queryParams) {
-
-    const meetingQueryParams = [req.query.meetingID ,req.query.meetingDate, req.query.meetingOutcomeAdvice, req.query.meetingOutcomeProgressionIssue];
-    
-    const meetingQLStrings = [
-      "meeting.meetingID = " + "'" + req.query.meetingID + "'",
-      "meeting.meetingDateTime = " + "'" + req.query.meetingDate + "'",
-      "meetingoutcome.adviceGivenID = " + "'" + req.query.meetingOutcomeAdvice + "'",
-      "meetingOutcome.progressionIssuesDiscussed = " + "'" + req.query.meetingOutcomeProgressionIssue + "'"
-        ]
-
-        const meetingParamsFiltered = meetingSQLStrings.filter(function(element, index){
-          if (!meetingQueryParams[index]) { return false } else return true;
-        })
-  }
-
-
-  const studentQueryParams = addStudentQueryParams(req.query);
-  const moduleQueryParams = addModuleQueryParams(req.query);
-  const programQueryParams = addProgramQueryParams(req.query);
-  const flagQueryParams = addFlagQueryParams(req.query);
-  const meetingQueryParams = addMeetingQueryParams(req.query);
+router.put("/students", async function (req, res, next) {
 
   try {
-    const students = await studentQueryHandler.getStudentsByQueryParams(studentQueryParams, moduleQueryParams, programQueryParams, flagQueryParams, meetingQueryParams, studentQueryHandler.getStudentsQueryBuilder);
-    res.send(students);
+    await studentQueryHandler.getStudentsQueryParamChecker(req);
+    res.send("OK");
   } catch (err) {
     next(err)
   }
 })
 
 module.exports = router;
-
-
-
-
-
-
-//GET STUDENTS BY A FLAG
-//GET STUDENTS BY PROGRAM (TERM)
-//GET STUDENTS BY ACADEMIC SUB PLAN
-//GET STUDENTS BY CURRENT LEVEL
-//GET STUDENTS BY ACADEMIC LOAD
-//GET STUDENTS BY CAREER (UG,PGT,PGR)
-
-//GET STUDENTS DETAILS
-//GET STUDENTS PROGRAM
-//GET STUDENTS MODULES
-//GET STUDENTS FLAGS
-//GET STUDENTS MODULE RESULT
-//GET STUDENTS MODULE RESULTS (ALL MODULE RESULTS)
-//GET STUDENTS MEETINGS
-
-//GET MODULES
-//GET MODULES BY CATALOGNUMBER
-//GET MODULES BY ASSESSMENT TYPE
-//GET MODULES BY PROGRAM (TERM)
-//GET MODULES BY CORE
-//GET MODULES BY SESSION
-
-//GET MODULES DETAILS
-//GET MODULES CATALOGNUMBER
-//GET MODULES DESCRIPTION
-//GET MODULES ASSESSMENT TYPE
-//GET MODULES SESSION
-//GET MODULES CORE
-
-//GET MEETINGS
-//GET MEETINGS BY STUDENT
-//GET MEETINGS BY ATTENDEE (STAFF/ADMIN)
-//GET MEETINGS BY DATE
-
-//GET MEETINGS DETAILS
-//GET MEETINGS
-//GET MEETINGS OUTCOME
-//GET MEETINGS ATTENDEES
-
-
-//GET FLAGS
-
-//GET PROGRAMS
-//GET PRGORAMS BY PROGRAM CODE
-//GET PROGRAMS BY ACADEMIC PLAN
-//GET PROGRAMS BY SUB ACADEMIC PLAN
-//GET PROGRAMS BY ACADEMIC ORG
-//GET PROGRAMS BY SUBJECT
-//GET PROGRAMS BY TERM
-//GET PROGRAMS BY CAREER
-//GET PROGRAMS BY ACADEMIC LOAD
-//GET PROGRAMS BY CAMPUS
-
-//GET PROGRAMS DETAILS
-//GET PROGRAMS ACADEMIC PLAN
-//GET PROGRAMS ACADEMIC SUB PLAN
-//GET PROGRAMS ACADEMIC ORG
-//GET PROGRAMS SUBJECT
-//GET PROGRAMS TERM
-//GET PROGRAMS CAREER
-//GET PROGRAMS ACADEMIC LOAD
-//GET PROGRAMS CAMPUS
